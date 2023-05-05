4dfdf7f54d681f6c48835e02a60a3ca785c5ec5b
nayem9b <razwanniam1@gmail.com>
2023-05-05 07:54:24 +0600

    asyncronous

---
+++ src/asyncronous.ts
@@ -0,0 +1,11 @@
+const makePromise= () : Promise<string>=>{
+    return new Promise<string>((resolve, reject)=>{
+        const data: string = "Data is fetched"
+        if(data){
+            resolve(data)
+        }else{
+            reject("Failed to fetch")
+        }
+    })
+}
+makePromise()
